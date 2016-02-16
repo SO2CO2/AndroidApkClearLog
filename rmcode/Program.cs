5630cb174c7d8d5d0e0121c0b9444daed7c072a9
SO2CO2 <shaofeng_sh@126.com>
2016-02-16 17:21:43 +0800

    clear Android Code Log
    clear Android Code Log

---
+++ rmcode/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading;
+
+namespace rmcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Usage: rmcode.exe inputDirectory keyword outputDirectory");
+                return;
+            }
+            string inputpath = args[0];
+            string outpath = args[2];
+            string p = Path.Combine(System.Environment.CurrentDirectory, outpath);
+            if (Directory.Exists(p))
+            {
+                // Delete the target to ensure it is not there.
+                Directory.Delete(p, true);
+                Thread.Sleep(1000);
+                Directory.CreateDirectory(p);
+            }
+            else
+            {
+                Directory.CreateDirectory(p);
+            }
+            QueryFiles.outdir = outpath;
+            QueryFiles.keystr = args[1];
+            QueryFiles.getFiles(inputpath);
+        }
+    }
+}
---
+++ rmcode/QueryFiles.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Collections;
+
+namespace rmcode
+{
+    class QueryFiles
+    {
+        public static string outdir = "";
+        public static string keystr = "";
+        public static Stack st = new Stack();
+        public static void getFiles(string path)
+        {
+            string[] strFileNames = Directory.GetFiles(path);
+            string[] strDirectories = Directory.GetDirectories(path);
+
+            foreach (string filename in strFileNames)
+            {
+                if (filename.EndsWith(".java"))
+                {
+                    QueryFileApi(filename);
+                }            
+            }
+            foreach (string dir in strDirectories)
+            {
+                Console.WriteLine("DirectoriesName:{0}", dir);
+                getFiles(dir);
+            }
+        }
+        public static void QueryFileApi(string fileName)
+        {
+            Console.WriteLine(fileName);
+            Directory.CreateDirectory(Path.Combine(outdir, Path.GetDirectoryName(fileName)));
+            st.Clear();
+            try
+            {
+                FileStream aFile = new FileStream(fileName, FileMode.Open);
+                StreamReader sr = new StreamReader(aFile);
+
+                var utf8WithoutBom = new System.Text.UTF8Encoding(false);
+                StreamWriter sw = new StreamWriter(Path.Combine(outdir, fileName), false, utf8WithoutBom);
+                string line = null;
+                string rmemptyline = null;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    rmemptyline = line.Trim();
+                    if (rmemptyline.StartsWith(keystr))
+                    {
+                        st.Clear();
+                        while (true)
+                        {
+                            StOp(rmemptyline);
+                            if (st.Count == 0 && rmemptyline.EndsWith(";"))
+                            {
+                                //说明这一句是一个完整的语法, 什么也不做，不再把这句写到新文件.
+                                break;
+                            }
+                            else
+                            {
+                                //说明换行了，再重新读一行
+                                line = sr.ReadLine();
+                                if (line != null)
+                                {
+                                    rmemptyline = line.Trim();
+                                }
+                                else
+                                {
+                                    rmemptyline = null;
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        sw.WriteLine(line);
+                        sw.Flush();
+                    }
+                }
+                sr.Close();
+                aFile.Close();
+                sw.Close();
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(fileName + ex.Message);
+            }
+        }
+        public static void StOp(string rmemptyline)
+        {
+            if (rmemptyline == null) return;
+            int i = 0;
+            for (i = 0; i < rmemptyline.Length; i++)
+            {
+                if (rmemptyline[i] == '(')
+                {
+                    if (st.Count == 0 || st.Peek().Equals("("))
+                    {
+                        st.Push("(");
+                    }
+                }
+                else if (rmemptyline[i] == '\"')
+                {
+                    if (i == 0)
+                    {
+                        if (st.Peek().Equals("\""))
+                        {
+                            st.Pop();
+                        }
+                        else
+                        {
+                            st.Push("\"");
+                        }
+                    }
+                    else
+                    {
+                        if (rmemptyline[i - 1] != ('\\'))
+                        {
+                            if (st.Peek().Equals("\""))
+                            {
+                                st.Pop();
+                            }
+                            else
+                            {
+                                st.Push("\"");
+                            }
+                        }
+                    }
+
+                }
+                else if (rmemptyline[i] == ')')
+                {
+                    if (st.Peek().Equals("("))
+                    {
+                        st.Pop();
+                    }
+                }
+            }
+        }
+    }
+}
